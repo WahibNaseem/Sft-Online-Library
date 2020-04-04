956da425744d10205930a7ea0de568d8c0d0c075
WahibAshraf <Wahib1900@gmail.com>
2020-04-04 12:30:18 +0800

    Book API completed

---
+++ SftLibrary.API/Resources/UserForListResource.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SftLibrary.API.Resources
+{
+    public class UserForListResource
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+    }
+}
